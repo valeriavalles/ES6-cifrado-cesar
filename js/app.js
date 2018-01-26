6701168737ea9c81f349bf578a5d5bb63111f320
valeriavalles <valeria.sv@outlook.es>
2018-01-25 21:51:28 -0500

    pasando a manual ES6-js

--- js/app.js
+++ js/app.js
@@ -1,96 +1,93 @@
-//Solicito una frase que será mi paramtero de entrada.
+window.addEventListener('load', function() {
+// Solicito una frase que será mi paramtero de entrada.
 
-let phraseText= prompt('Ingrese una frase');
+// let phraseText= prompt('Ingrese una frase');
 
-/*var cifrado = document.getElementById('cifrado-text');
-var btn = document.getElementById('btn-click');*/
+  let cifrado = document.getElementById('cifrado-text');
+  let btn = document.getElementById('btn-click');
+  let cipherText = document.getElementById('cipher-text');
+  let t = cifrado.value;
 
-//Valido que si no ingresa ninguna cadena me vuelva a pedir
-
-  while( isNaN(phraseText) !== true || phraseText===' ' ) {
-    // alert('Error. Intente nuevamente.');
-     
-     let phraseText= prompt('Ingrese una frase');
-     
-   }
-
-
-/*btn.addEventListener('click', function(){
-  var t = cifrado.value;
-  document.write('La frase ingresada es:  '+t);
- // console.log(t);
-  cifrado.value = '';
- });*/
+   
+  btn.addEventListener('click', (event) => {
+    let t = cifrado.value;
+    cipherText.innerHTML = t;
+  
+    // console.log(t);
+    cifrado.value = '';
+  });
 
 
-//Muestro la frase ingresada
-//document.write('La frase ingresada es:  '+text);
+  // Muestro la frase ingresada
+  // document.write('La frase ingresada es:  '+text);
 
-//Creando mi función cifrado
-let cipher = (phrase) => {
+  // Creando mi función cifrado
+  const cipher = (phrase) => {
+  // declaro las variables que utilizaré para almacenar mis valores
+    let codAscii;
+    let cipCesar;
+    let newPhrase = '';
 
-  //declaro las variables que utilizaré para almacenar mis valores
-  let codAscii;
-  let cipCesar;
-  let newPhrase="";
+    // Bucle que me sirve para obtener convertir mi frase y cifrarla.
+    for (let i = 0; i < phrase.length; i++) {
+    // Obtengo el código ascii de la letra en su posición i
+      codAscii = phrase.charCodeAt(i);
+      // Verifico si la frase ingresada está escrita en mayúscula
+      if (phrase.toUpperCase() === phrase) {
+        // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las mayúsculas
+        cipCesar = (codAscii - 65 + 33) % 26 + 65;
+      } else {
+        // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las minúsculas
+        cipCesar = (codAscii - 97 + 33) % 26 + 97;
+      }
 
-  //Bucle que me sirve para obtener convertir mi frase y cifrarla.
-  for(let i=0; i<phrase.length; i++){
-    //Obtengo el código ascii de la letra en su posición i
-    codAscii=phrase.charCodeAt(i);
-    //Verifico si la frase ingresada está escrita en mayúscula
-    if(phrase.toUpperCase()===phrase){
-    // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las mayúsculas
-     cipCesar=(codAscii-65+33)%26+65;
-   }else{
-    //Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las minúsculas
-     cipCesar=(codAscii-97+33)%26+97;
-   }
-
-   //Convierte el codigo ASCII a una letra
-    let cad=String.fromCharCode(cipCesar);
-  //Concatena las letras que van llegando en cada iteración y lo almacena en mi variable
-    newPhrase=newPhrase.concat(cad);
-  }
-  //Retorna la cadena total de todas las letras convertidas
-  return newPhrase;
-}
-
-//Muestra la frase cifrada
-document.write('<br>' +'La frase cifrada es:  '+ cipher(phraseText));
-
-let newPhrase=cipher(phraseText);
-
-//Creando mi función descifrar;
-let decipher = (newPhrase) => {
-  let acum;
-  let valor;
-  let descipherPhrase='';
-//Bucle que me sirve para obtener convertir mi frase y descifrarla.
-  for(var i=0; i<newPhrase.length; i++){
-    //Obtengo el código ascii de la letra en su posición i
-    acum=newPhrase.charCodeAt(i);
-    //Verifico si la frase ingresada está escrita en mayúscula
-    if(newPhrase.toUpperCase()===newPhrase){
-      // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las mayúsculas
-      valor=(acum+65+33)%26+65;
-    }else{
-      // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las minúsculas
-      valor=(acum+97+33)%26+97;
+      // Convierte el codigo ASCII a una letra
+      let cad = String.fromCharCode(cipCesar);
+      // Concatena las letras que van llegando en cada iteración y lo almacena en mi variable
+      newPhrase = newPhrase.concat(cad);
     }
+    // Retorna la cadena total de todas las letras convertidas
+    return newPhrase;
+  };
 
-    //Convierte el codigo ASCII a una letra
-    let str=String.fromCharCode(valor);
-    //Concatena las letras que van llegando en cada iteración y lo almacena en mi variable
-    descipherPhrase=descipherPhrase.concat(str);
-  }
-  //Retorna la frase descifrada
-  return descipherPhrase;
-}
-//Muestro la frase descifrada en la página
-document.write('<br>' +'La frase decifrada es:  '+ decipher(newPhrase));
+  // Muestra la frase cifrada
+  cipherText.innerHTML = '<br>' + 'La frase cifrada es:  ' + t;
+  // document.write('<br>' +'La frase cifrada es:  '+ cipher(cifrado.value));
 
-console.log(phraseText);
-console.log(cipher(phraseText));
-console.log(decipher(newPhrase));
 
+  let newPhrase = cipher(t);
+
+
+  // Creando mi función descifrar;
+  let decipher = (newPhrase) => {
+    let acum;
+    let valor;
+    let descipherPhrase = '';
+    // Bucle que me sirve para obtener convertir mi frase y descifrarla.
+    for (var i = 0; i < newPhrase.length; i++) {
+    // Obtengo el código ascii de la letra en su posición i
+      acum = newPhrase.charCodeAt(i);
+      // Verifico si la frase ingresada está escrita en mayúscula
+      if (newPhrase.toUpperCase() === newPhrase) {
+      // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las mayúsculas
+        valor = (acum + 65 + 33) % 26 + 65;
+      } else {
+      // Me convierte la letra que tenia en ascii a cifrado de cesar; esta formula se utiliza para las minúsculas
+        valor = (acum + 97 + 33) % 26 + 97;
+      }
+
+      // Convierte el codigo ASCII a una letra
+      let str = String.fromCharCode(valor);
+      // Concatena las letras que van llegando en cada iteración y lo almacena en mi variable
+      descipherPhrase = descipherPhrase.concat(str);
+    }
+    // Retorna la frase descifrada
+    return descipherPhrase;
+  };
+  // Muestro la frase descifrada en la página
+  // document.write('<br>' + 'La frase decifrada es:  ' + decipher(t));
+
+  console.log(t);
+  console.log(cipher(t));
+  console.log(decipher(t));
+});
